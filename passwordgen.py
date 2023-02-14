fde26dd8e8761f9483743a3f73e19c91ecc88575
chaotic-mo <motesraji@gmail.com>
2023-02-14 22:03:02 +0100

    first part password

---
+++ passwordgen.py
@@ -0,0 +1,33 @@
+'''
+Maak een programma die een wachtwoord van 24 tekens genereerd.
+Het wachtwoord moet aan de volgende eisen voldoen:
+    # 2 tot 6 hoofdletters.
+    # Minimaal 8 kleine letters.
+    # 3 speciale tekens uit de volgende reeks: @ # $ % & _ ?.
+    De speciale tekens mogen niet op de eerste of laatste positie staan en ook niet op een vaste plek.
+    4 tot 7 cijfers (0 t/m 9).
+    Op de eerste 3 posities mag geen cijfer staan.
+'''
+# necessary imports 
+import string, random
+# defining the strings
+lettersu = string.ascii_uppercase
+lettersc = string.ascii_lowercase
+tekens = ['@','#','$','&','_','?','.']
+cijfers = string.digits
+# pwd lenght
+pwd_lenght = 24
+# generate a pwd_string
+pwd = []
+
+for i in range(random.randrange(2, 6)):
+    pwd.append(lettersu[random.randrange(len(lettersu))])
+for i in range(3):
+    pwd.append(tekens[random.randrange(len(tekens))])
+for i in range(random.randrange(4, 7)):
+    pwd.append(cijfers[random.randrange(len(cijfers))])
+for i in range(pwd_lenght - len(pwd)):
+    pwd.append(lettersc[random.randrange(len(lettersc))])
+
+print(pwd)
+print(len(pwd))
